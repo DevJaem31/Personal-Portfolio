970b2f3d2e94f3ca617a865e682332a39f72378c
John Mark E. Flameno <johnmarkflameno@gmail.com>
2025-03-24 11:07:24 +0800

    Revision of Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import MainPage from './pages/main-page/main-page';
+
+function App() {
+	return (
+		<div>
+			<MainPage />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main-page.css';
+
+function MainPage() {
+	return (
+		<div className='main-page-container'>
+			<div className='left-side-container'>Left</div>
+			<div className='right-side-container'>Right</div>
+		</div>
+	);
+}
+
+export default MainPage;
