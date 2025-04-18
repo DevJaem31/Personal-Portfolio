4df517d1d9683e2dac7df1230b294f8200cdf402
John Mark E. Flameno <177906847+DevJaem31@users.noreply.github.com>
2025-04-18 18:36:34 +0800

    Optimizing my code for my portfolio

--- src/sections/skills-section/skills-section.jsx
+++ src/sections/skills-section/skills-section.jsx
@@ -37,79 +37,7 @@ function SkillsSection() {
 					</h2>
 				</div>
 
-				<div className='skills-container-additional grid grid-cols-4 gap-y-10 items-center gap-5 grid-rows-subgrid justify-center grid-rows-auto gap-2 h-fit'>
-					<div className='skills-item flex flex-col items-center mt-3 gap-2'>
-						<img
-							src={PythonIcon}
-							alt='Python'
-							className='skills-icon ratio-square h-[50px] grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Python</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={JavaIcon}
-							alt='Java'
-							className='skills-icon ratio-square h-[64px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-1'>Java</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center mt-3.5 gap-2'>
-						<img
-							src={CplusplusIcon}
-							alt='C++'
-							className='skills-icon ratio-square h-[50px] mb-0.5 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>C++</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center mt-3.5 gap-2'>
-						<img
-							src={TypescriptIcon}
-							alt='Typescript'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Typescript</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={AngularIcon}
-							alt='Angular'
-							className='skills-icon ratio-square h-[64px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Angular</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={DjangoIcon}
-							alt='Django'
-							className='skills-icon ratio-square h-[64px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Django</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={CiscoIcon}
-							alt='Cisco'
-							className='skills-icon ratio-square h-[64px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name'>Cisco</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={SalesforceIcon}
-							alt='Salesforce'
-							className='skills-icon ratio-square h-[64px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Salesforce</p>
-					</div>
-				</div>
+				<div className='skills-container-additional grid grid-cols-4 gap-y-10 items-center gap-5 grid-rows-subgrid justify-center grid-rows-auto gap-2 h-fit'></div>
 			</div>
 
 			<div className='tools-used-container mt-20'>
@@ -119,61 +47,7 @@ function SkillsSection() {
 					</h2>
 				</div>
 
-				<div className='skills-container-tools grid grid-cols-3 gap-y-10 items-center gap-5 grid-rows-subgrid justify-center grid-rows-auto gap-2 h-fit'>
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={VSCodeIcon}
-							alt='Visual Studio Code'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Visual Studio Code</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={AndroidIcon}
-							alt='Android Studio'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name ml-2'>Android Studio</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={EclipseIcon}
-							alt='Eclipse IDE'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name'>Eclipse IDE</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={PostmanIcon}
-							alt='Postman'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name'>Postman</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={GithubIcon}
-							alt='Github'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name'>Github</p>
-					</div>
-
-					<div className='skills-item flex flex-col items-center gap-2'>
-						<img
-							src={MavenIcon}
-							alt='Maven'
-							className='skills-icon ratio-square h-[50px] m-0 grayscale opacity-50'
-						/>
-						<p className='skills-name'>Maven</p>
-					</div>
-				</div>
+				<div className='skills-container-tools grid grid-cols-3 gap-y-10 items-center gap-5 grid-rows-subgrid justify-center grid-rows-auto gap-2 h-fit'></div>
 			</div>
 		</div>
 	);
