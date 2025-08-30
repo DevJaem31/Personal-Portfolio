43671514b3fcc41999a71cffbf2992629ae6f447
John Mark E. Flameño <177906847+DevJaem31@users.noreply.github.com>
2025-08-30 19:03:23 +0800

    Update experience-section.jsx

--- src/sections/experience-section/experience-section.jsx
+++ src/sections/experience-section/experience-section.jsx
@@ -13,6 +13,37 @@ function ExperienceSection() {
 				<div className='experience-item-container grid grid-cols-4 gap-y-5 items-center opacity-[0.6] '>
 					<div className='date-container flex justify-center text-center'>
 						<p className='date-text text-center text-xs tracking-widest text-[var(--inactive-text)]'>
+							JULY 2025 — PRESENT
+						</p>
+					</div>
+
+					<div className='experience-title-container col-span-3 flex gap-1'>
+						<a
+							href='https://www.accenture.com/ph-en'
+							target='_blank'
+							rel='noopener noreferrer'
+							className='title-text-experience font-bold text-lg text-white tracking-wider'
+						>
+							Associate Software Engineer • Accenture Philippines
+						</a>
+						<img
+							src={arrowDiag}
+							alt=''
+							className='aspect-square h-[20px] opacity-[0.8] grayscale mt-1 exp-arrow'
+						/>
+					</div>
+
+					<div className='description-container col-span-3 col-start-2'>
+						<p className='description-text text-justify text-sm tracking-widest font-stretch-semi-expanded font-extralight pr-3 text-white opacity-80'>
+							Currently under package app development, and already finished ATAS of 
+							<span>Accenture Philippines</span> and currently taking Java Full-Stack Booth Camp.
+						</p>
+					</div>
+				</div>
+				
+				<div className='experience-item-container grid grid-cols-4 gap-y-5 items-center opacity-[0.6] '>
+					<div className='date-container flex justify-center text-center'>
+						<p className='date-text text-center text-xs tracking-widest text-[var(--inactive-text)]'>
 							JAN — APR 2025
 						</p>
 					</div>
