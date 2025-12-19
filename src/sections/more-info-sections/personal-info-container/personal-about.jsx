94606d81d9b57898fa84d7eb6a1f4798bfd3f14d
DevJaem31 <177906847+DevJaem31@users.noreply.github.com>
2025-12-19 20:42:30 +0800

    updated my resume

--- src/sections/more-info-sections/personal-info-container/personal-about.jsx
+++ src/sections/more-info-sections/personal-info-container/personal-about.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './personal-about.css';
-import pin from '../../../assets/icons/pin.png';
-import profilePic from '../../../assets/Jaem.png';
+import React from "react";
+import "./personal-about.css";
+import pin from "../../../assets/icons/pin.png";
+// import profilePic from '../../../assets/Jaem.png';
 
 function PersonalAboutSection({ onNavigate }) {
-	return (
-		<div className='personal-about-container m-auto'>
-			<div className='personal-grid-container flex w-[70%] m-auto flex-row  gap-5'>
-				{/* <div className='personal-image-container flex col-span-2'>
+  return (
+    <div className="personal-about-container m-auto">
+      <div className="personal-grid-container flex w-[70%] m-auto flex-row  gap-5">
+        {/* <div className='personal-image-container flex col-span-2'>
 					<img
 						src={profilePic}
 						alt=''
@@ -15,34 +15,38 @@ function PersonalAboutSection({ onNavigate }) {
 					/>
 				</div> */}
 
-				<div className='personal-details-container col-span-2 flex flex-col justify-center'>
-					<div className='location-container mb-2 flex flex-row gap-1 items-center'>
-						<img
-							src={pin}
-							alt='location pin'
-							className='ratio-square h-[20px] opacity-50 location-pin'
-						/>
-						<p className='location-text mt-1 font-semibold'>Bulacan, Philippines</p>
-					</div>
+        <div className="personal-details-container col-span-2 flex flex-col justify-center">
+          <div className="location-container mb-2 flex flex-row gap-1 items-center">
+            <img
+              src={pin}
+              alt="location pin"
+              className="ratio-square h-[20px] opacity-50 location-pin"
+            />
+            <p className="location-text mt-1 font-semibold">
+              Bulacan, Philippines
+            </p>
+          </div>
 
-					<h1 className='personal-name text-5xl font-bold'>
-						<a
-							href='/more-info'
-							onClick={(e) => {
-								e.preventDefault(); // Prevent page reload
-								window.history.pushState({}, '', '/more-info'); // Update URL
-								onNavigate('more-info'); // Change the page state
-							}}
-						>
-							John Mark Flameño
-						</a>
-					</h1>
+          <h1 className="personal-name text-5xl font-bold">
+            <a
+              href="/more-info"
+              onClick={(e) => {
+                e.preventDefault(); // Prevent page reload
+                window.history.pushState({}, "", "/more-info"); // Update URL
+                onNavigate("more-info"); // Change the page state
+              }}
+            >
+              John Mark Flameño
+            </a>
+          </h1>
 
-					<p className='work-preference text-xl mt-2 font-semibold '>Full Stack Developer</p>
-				</div>
-			</div>
-		</div>
-	);
+          <p className="work-preference text-xl mt-2 font-semibold ">
+            Full Stack Developer
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PersonalAboutSection;
